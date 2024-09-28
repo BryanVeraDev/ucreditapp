fd50daf9ddfec39b168397d1c22a8534f6242ee2
BryanDev <bryanalejandrovo@ufps.edu.co>
2024-09-27 21:26:40 -0500

    fix: rename 'name' field to 'description' in Status models

--- status/models.py
+++ status/models.py
@@ -2,21 +2,21 @@ from django.db import models
 
 class StatusGeneral(models.Model):
     id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=50)
     
     def __str__(self):
-        return self.name
+        return self.description
     
 class StatusCredit(models.Model):
     id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=20)
+    description = models.CharField(max_length=20)
     
     def __str__(self):
-        return self.name
+        return self.description
 
 class StatusPayment(models.Model):
     id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=20)
+    description = models.CharField(max_length=20)
     
     def __str__(self):
-        return self.name      
+        return self.description      
