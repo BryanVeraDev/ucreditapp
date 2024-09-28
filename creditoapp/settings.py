54125a652f95637a7677bd5c5dcc24e2e86ff77d
saimers <saimeradriansr@ufps.edu.co>
2024-09-27 21:18:11 -0500

    feat: add Product model

--- creditoapp/settings.py
+++ creditoapp/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'status',
-    'users'
+    'users',
+    'products'
 ]
 
 MIDDLEWARE = [
---
+++ products/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from status.models import StatusGeneral
+
+class Product(models.Model):
+    id = models.CharField(max_length=12, primary_key=True)
+    name = models.CharField(max_length=30)
+    description = models.CharField(max_length=50)
+    price = models.DecimalField(max_digits=11, decimal_places=2)
+    status = models.ForeignKey(StatusGeneral, on_delete=models.CASCADE)
+    product_type = models.ForeignKey('ProductType', on_delete=models.CASCADE)
+    
+    def __str__(self) -> str:
+        return self.name
+
+class ProductType(models.Model):
+    id = models.AutoField(primary_key=True)
+    description = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.description
